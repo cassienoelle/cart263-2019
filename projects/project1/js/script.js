94a6a594f4cb1a1c87ab6e81105ea2cb4de88897
cassienoelle <cassienoelle@gmail.com>
2019-01-30 21:40:55 -0500

    P1: updated css, added :hover for keys, started key click animation in script.js

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -3,7 +3,41 @@
 Placeholder Title
 Cassie Smith
 
-An absurdist project inspired by the myth of Sisyphus.
-Navigate through the hold menu options in order to [insert goal here].
+An absurdist project inspired by Camus' The Myth of Sisyphus.
+Navigate through the phone menu options in order to [insert goal here].
+
+Lose if you press zero (take a leap of faith and defer to belief in God)
+or hang up (give up and commit suicide).
 
 ******************/
+"use strict";
+
+// array representing the keypad
+let $keys = [];
+// variable to select all divs with key class
+let $key = $('.key');
+
+// variables to hold currently selected key
+let $selectedKey = undefined;
+
+
+// call the setup function when document is loaded
+$(document).ready(setup);
+
+// setup()
+//
+//
+function setup() {
+  $key.on('click', selected);
+
+}
+
+// selected()
+//
+//
+function selected() {
+  // set $selectedKey
+
+  // animate key (flash a different color)
+
+}
