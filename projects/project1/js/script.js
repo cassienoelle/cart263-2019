be819d2ca0883af23379e845bbec56ef7ac76887
cassienoelle <cassienoelle@gmail.com>
2019-01-31 17:35:17 -0500

    P1: added large button, functions to set responsive interface
    responsiveInterface()
    calcString()

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -15,29 +15,88 @@ or hang up (give up and commit suicide).
 // array representing the keypad
 let $keys = [];
 // variable to select all divs with key class
-let $key = $('.key');
+let $key;
+let $smallKey;
+let $largeKey;
+
+// variables to set height of keys
+let $smallKeyWidth;
+let $smallKeyHeight;
+let $largeKeyHeight;
 
 // variables to hold currently selected key
 let $selectedKey = undefined;
 
 
-// call the setup function when document is loaded
-$(document).ready(setup);
+$(document).ready(function() {
+  $key = $('.key');
+  $smallKey = $('.small');
+  $largeKey = $('.large');
 
-// setup()
+  responsiveInterface();
+
+  $key.on('click', function() {
+    let $selectedKey = $(this);
+  });
+
+});
+
+
+// responsiveInterface()
 //
 //
-function setup() {
-  $key.on('click', selected);
+function responsiveInterface() {
+  // set key height relative to responsive width
+
+  // set small key width the same as small key height
+  $smallKeyWidth = $smallKey.css('width');
+  $smallKeyHeight = $smallKeyWidth;
+  $smallKey.css('height', $smallKeyHeight);
+
+  // set large key height at half of small key height
+  // (sets height automatically with line-height)
+  $largeKeyHeight = calcString($smallKeyHeight, 'px', 'divide', 2);
+  $largeKey.css('line-height', $largeKeyHeight);
+
+
+  console.log('small width: ' + $smallKeyWidth);
+  console.log('small height: ' + $smallKeyHeight);
+  console.log('large height: ' + $largeKeyHeight);
+  console.log('line-height: ' + $largeKey.css('line-height'));
 
 }
 
-// selected()
+// calcString()
 //
-//
-function selected() {
-  // set $selectedKey
+// perform calculations on css values represented as strings
+function calcString(value, suffix, operation, number) {
+  // remove suffix from original string
+  let $subStr = value.substring(0, value.length - 2);
+  // convert string to number
+  let $newValue = Number($subStr);
 
-  // animate key (flash a different color)
+  // perform specified operation
+  switch(operation) {
+    case 'divide':
+      $newValue = $newValue / number;
+      break;
+    case 'multiply':
+      $newValue = $newValue * number;
+      break;
+    case 'add':
+      $newValue = $newValue + number;
+      break;
+    case 'subtract':
+      $newValue = $newValue - number;
+      break;
+    default:
+      break;
+  }
 
+  // convert number back to string and re-add suffix
+  $newValue = String($newValue) + suffix;
+  console.log($newValue);
+
+  // return new string value
+  return $newValue;
 }
