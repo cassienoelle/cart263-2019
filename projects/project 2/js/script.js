6f35978856557072da8e29e2fc13cfb1f404d3b5
cassienoelle <cassienoelle@gmail.com>
2019-03-06 18:03:53 -0500

    P2: setup canvas and started exerimenting with basic colour coded shapes

--- projects/project 2/js/script.js
+++ projects/project 2/js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 /*****************
 
 Title of Project
@@ -7,3 +8,55 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+let width = 1000;
+let height = 1000;
+
+let config = {
+    type: Phaser.AUTO,
+    width: width,
+    height: height,
+    scene: {
+      create: create
+    }
+};
+
+let game = new Phaser.Game(config);
+
+//----------------------------
+
+let rectOne;
+let rectTwo;
+let rectThree;
+let rectFour;
+
+let green = 0xffeb32;
+let red = 0xee3321;
+let blue = 0x2152ee;
+let yellow = 0x54bf4f;
+
+let rectWidth = width/2;
+let rectHeight = height/2;
+
+//----------------------------
+
+function preload () {
+
+}
+
+function create () {
+  rectOne = new Phaser.Geom.Rectangle(0, 0, rectWidth, rectHeight);
+  rectTwo = new Phaser.Geom.Rectangle(width/2, 0, rectWidth, rectHeight);
+  rectThree = new Phaser.Geom.Rectangle(0, height/2, rectWidth, rectHeight);
+  rectFour = new Phaser.Geom.Rectangle(width/2, height/2, rectWidth, rectHeight)
+
+  let graphicsOne = this.add.graphics({ fillStyle: {color: red} });
+  let graphicsTwo = this.add.graphics({ fillStyle: {color: blue} });
+  let graphicsThree = this.add.graphics({ fillStyle: {color: green} });
+  let graphicsFour = this.add.graphics({ fillStyle: {color: yellow} });
+
+  graphicsOne.fillRectShape(rectOne);
+  graphicsTwo.fillRectShape(rectTwo);
+  graphicsThree.fillRectShape(rectThree);
+  graphicsFour.fillRectShape(rectFour);
+};
