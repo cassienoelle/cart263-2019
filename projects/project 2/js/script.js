9a806876de312f46d0170abacd42f3e29c94ce80
cassienoelle <cassienoelle@gmail.com>
2019-03-07 15:02:57 -0500

    P2: set up pixi app / canvas

--- projects/project 2/js/script.js
+++ projects/project 2/js/script.js
@@ -9,9 +9,33 @@ author, and this description to match your project!
 
 ******************/
 let type = "WebGL";
-
 if(!PIXI.utils.isWebGLSupported()){
   type = "canvas"
 }
-
 PIXI.utils.sayHello(type);
+
+let app;
+
+window.onload = function() {
+  // Create Pixi app and stage
+    app = new PIXI.Application({
+    width: 800,
+    height: 600,
+    antialias: true,
+    backgroundColor: 0x000000
+  });
+
+  document.body.appendChild(app.view);
+
+  // Snap canvas size to window size
+  app.renderer.view.style.position = "absolute";
+  app.renderer.view.style.display = "block";
+  resize();
+  window.onresize = resize;
+}
+
+// Resize canvas to window size
+function resize() {
+  app.renderer.autoResize = true;
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+}
