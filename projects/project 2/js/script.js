4b5ad5e2abeedc6dd806922e5bf72d55ca52d363
cassienoelle <cassienoelle@gmail.com>
2019-03-07 17:05:20 -0500

    P2: set base circle interface relative to window size

--- projects/project 2/js/script.js
+++ projects/project 2/js/script.js
@@ -14,17 +14,15 @@ if(!PIXI.utils.isWebGLSupported()){
 }
 PIXI.utils.sayHello(type);
 
-let app;
-
-window.onload = function() {
-  // Create Pixi app and stage
-    app = new PIXI.Application({
+// Create Pixi app
+let app = new PIXI.Application({
     width: 800,
     height: 600,
     antialias: true,
     backgroundColor: 0x000000
-  });
+});
 
+window.onload = function() {
   document.body.appendChild(app.view);
 
   // Snap canvas size to window size
@@ -34,8 +32,60 @@ window.onload = function() {
   window.onresize = resize;
 }
 
-// Resize canvas to window size
+
+
+//------------------------------
+
+let circle = new PIXI.Graphics();
+let rad;
+
+if (window.innerWidth >= window.innerHeight) {
+  rad = (window.innerHeight * 0.9) / 2;
+} else if (window.innerHeight > window.innerWidth) {
+  rad = (window.innerHeight * 0.9) / 2;
+}
+
+circle.beginFill(0xE9EBEE);
+circle.drawCircle(0, 0, rad);
+circle.x = window.innerWidth;
+circle.y = window.innerHeight;
+circle.endFill();
+
+app.stage.addChild(circle);
+
+//-------------------------------
+
+
+// Resize canvas to window size and set width / height variables
 function resize() {
   app.renderer.autoResize = true;
   app.renderer.resize(window.innerWidth, window.innerHeight);
+  circle.x = window.innerWidth/2;
+  circle.y = window.innerHeight/2;
 }
+
+
+//-------------------------------
+
+
+let graphics = new PIXI.Graphics();
+
+// set a fill and line style again
+graphics.lineStyle(10, 0xFF0000, 0.8);
+graphics.beginFill(0xFF700B, 1);
+
+// Draw a shape
+graphics.moveTo()
+
+
+// graphics.moveTo(210,300);
+// graphics.lineTo(450,320);
+// graphics.lineTo(570,350);
+// graphics.quadraticCurveTo(600, 0, 480,100);
+// graphics.lineTo(330,120);
+// graphics.lineTo(410,200);
+// graphics.lineTo(210,300);
+graphics.endFill();
+
+
+app.stage.addChild(graphics);
