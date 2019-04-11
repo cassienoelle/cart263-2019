0196f9ea8565c4598812b434ea3bbba41745efb3
cassienoelle <cassienoelle@gmail.com>
2019-04-11 19:17:37 -0400

    P3: removed tracking.js added posenet

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -10,57 +10,40 @@ Cassie Smith
 
 ******************/
 
-
-let colors = new tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
-
- colors.on('track', function(event) {
-   if (event.data.length === 0) {
-     // No colors were detected in this frame.
-   } else {
-     event.data.forEach(function(rect) {
-       console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
-     });
-   }
- });
-
-tracking.track('#myVideo', colors);
+let canvas, capture;
 
 
-// let canvas, capture;
+// preload()
 //
 //
-// // preload()
-// //
-// //
-//
-// function preload() {
-//
-// }
-//
-//
-// // setup()
-// //
-// //
-//
-// function setup() {
-//
-//   canvas = createCanvas(640,480);
-//   background(0);
-//   capture = createCapture(VIDEO);
-//   capture.size(640, 480);
-//   capture.hide();
-//
-// }
+
+function preload() {
+
+}
+
+
+// setup()
 //
 //
-// // draw()
-// //
-// //
+
+function setup() {
+
+  canvas = createCanvas(640,480);
+  background(0);
+  capture = createCapture(VIDEO);
+  capture.size(640, 480);
+  capture.hide();
+
+}
+
+
+// draw()
 //
-// function draw() {
 //
-//   background(0);
-//   tint(255, 0, 250);
-//   image(capture, 0, 0);
-//
-// }
+
+function draw() {
+
+  tint(255, 0, 250);
+  image(capture, 0, 0);
+
+}
